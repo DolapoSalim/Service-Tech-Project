266bcb597436509badc6b0175de799362c9da729
Dolapo Olatoye <dolaposalim@gmaail>
2023-08-12 12:34:35 +0200

    Latest commit. Files added and paths changed

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Service-Tech-Project
+This project is a dummy project for my portfolio, for the design of a tech service website
+
+## Acknowledgements
+
+ - [Inspiration](https://pinterest.com)
+
+ ## Authors
+
+- [@dolaposalim](https://www.github.com/dolaposalim)
+
+## Tech Stack
+
+**Client:** React, VanillaCSS, Awesome Font
+
+**Server:** Node
---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+
+    var el = document.createElement("div");
+    el.innerHTML = "RABBIT TEST ";
+    var div = document.getElementById("headline");
+    insertAfter(div, el);
+
+    /* FUNCTIONS */
+
+    // Inserts newNode after referenceNode
+    function insertAfter(referenceNode, newNode) {
+        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    }
+};
+
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
